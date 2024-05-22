7c10f49957065793d434885124b31d21d1763bd2
lourenxx <guilhermepxl7@gmail.com>
2024-05-22 17:10:15 -0300

    finalizando script python e adicionando arquivo xlsx

--- grafico.py
+++ grafico.py
@@ -1,20 +1,38 @@
+from openpyxl import load_workbook
+
 
 def ler_dados_arquivo(nome_arquivo):
     with open(nome_arquivo, 'r') as arquivo:
         linhas = arquivo.readlines()
         altura = None
-        tempo = None
-        velocidade_inicial_em_y = None
+        velocidade_inicial = None
+        tangente_minima = None
+
         for linha in linhas:
             if linha.startswith("Altura do alvo:"):
-                altura = float(linha.split(": ")[1].strip())
+                altura = (linha.split(": ")[1].strip())
                 print(altura)
+                
 
-            elif linha.startswith("Tempo gasto para atingir o alvo:"):
-                tempo = float(linha.split(": ")[1].strip().replace(",", "."))
-                print(tempo)
+            elif linha.startswith("Velocidade inicial do projétil:"):
+                velocidade_inicial = (linha.split(": ")[1].strip())
+                print(velocidade_inicial)
+                
 
-        return altura, tempo
+            elif linha.startswith("Tangente mínima para atingir o alvo:"):
+                tangente_minima = (linha.split(": ")[1].strip())
+                print(tangente_minima)
+
+        return altura, velocidade_inicial, tangente_minima
     
-ler_dados_arquivo("dados.txt")
     
+altura, velocidade_incial, tangente_minima = ler_dados_arquivo("dados.txt")
+
+ler_planilha = load_workbook('graficoTrajetoria.xlsx')
+ativar_planilha = ler_planilha.active
+
+ativar_planilha['C2'] = velocidade_incial
+ativar_planilha['D2'] = tangente_minima 
+
+
+ler_planilha.save('graficoTrajetoria.xlsx')
