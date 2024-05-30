5cfa381e10f1803f746df11935b313350982c5c5
lourenxx <guilhermepxl7@gmail.com>
2024-05-30 02:46:01 -0300

    mudanças não feitas

--- grafico.py
+++ grafico.py
@@ -4,35 +4,37 @@ from openpyxl import load_workbook
 def ler_dados_arquivo(nome_arquivo):
     with open(nome_arquivo, 'r') as arquivo:
         linhas = arquivo.readlines()
-        altura = None
+        altura_alvo = None
         velocidade_inicial = None
         tangente_minima = None
+        aceleracao_gravidade = None
 
         for linha in linhas:
             if linha.startswith("Altura do alvo:"):
-                altura = (linha.split(": ")[1].strip())
-                print(altura)
+                altura_alvo = float(linha.split(": ")[1].strip().replace(',', '.'))
+                print(altura_alvo)
                 
-
             elif linha.startswith("Velocidade inicial do projétil:"):
-                velocidade_inicial = (linha.split(": ")[1].strip())
+                velocidade_inicial = float(linha.split(": ")[1].strip().replace(',', '.'))
                 print(velocidade_inicial)
                 
-
             elif linha.startswith("Tangente mínima para atingir o alvo:"):
-                tangente_minima = (linha.split(": ")[1].strip())
+                tangente_minima = float(linha.split(": ")[1].strip().replace(',', '.'))
                 print(tangente_minima)
+            
+            elif linha.startswith("Aceleração da Gravidade:"):
+                aceleracao_gravidade = float(linha.split(": ")[1].strip().replace(',', '.'))
+                print(aceleracao_gravidade)
 
-        return altura, velocidade_inicial, tangente_minima
-    
-    
-altura, velocidade_incial, tangente_minima = ler_dados_arquivo("dados.txt")
+        return altura_alvo, velocidade_inicial, tangente_minima, aceleracao_gravidade
+
+altura_alvo, velocidade_inicial, tangente_minima, aceleracao_gravidade = ler_dados_arquivo("dados.txt")
 
 ler_planilha = load_workbook('graficoTrajetoria.xlsx')
 ativar_planilha = ler_planilha.active
 
-ativar_planilha['C2'] = velocidade_incial
-ativar_planilha['D2'] = tangente_minima 
-
+ativar_planilha['C2'] = velocidade_inicial
+ativar_planilha['D2'] = tangente_minima
+ativar_planilha['E2'] =  altura_alvo
 
 ler_planilha.save('graficoTrajetoria.xlsx')
